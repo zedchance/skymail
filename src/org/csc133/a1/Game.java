51a5893146c41e73da697854c5a2f2d81130c333
Zed Chance <zchance@csus.edu>
2021-02-17 17:18:14 -0800

    Initial commit
    Boiler plate setup and key commands shell in place

---
+++ src/org/csc133/a1/Game.java
@@ -0,0 +1,94 @@
+package org.csc133.a1;
+
+import com.codename1.ui.Form;
+import com.codename1.ui.Label;
+import com.codename1.ui.TextField;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
+import org.w3c.dom.Text;
+
+public class Game extends Form
+{
+    private GameWorld gw;
+
+    public Game()
+    {
+        System.out.println("New game started");
+        gw = new GameWorld();
+        gw.init();
+        play();
+    }
+
+    private void play()
+    {
+        Label lblCommand = new Label("Enter a command: ");
+        addComponent(lblCommand);
+        final TextField txtInput = new TextField();
+        addComponent(txtInput);
+        this.show();
+
+        // accept and execute user command
+        txtInput.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent evt)
+            {
+                String sCommand = txtInput.getText().toString();
+                if (sCommand == null || sCommand.equals(""))
+                {
+                    return;
+                }
+                txtInput.clear();
+                switch (sCommand.charAt(0))
+                {
+                    case 'A':
+                    case 'a':
+                        System.out.println("Accelerating");
+                        break;
+                    case 'B':
+                    case 'b':
+                        System.out.println("Braking");
+                        break;
+                    case 'L':
+                    case 'l':
+                        System.out.println("Turning left");
+                        break;
+                    case 'R':
+                    case 'r':
+                        System.out.println("Turning right");
+                        break;
+                    case 'C':
+                    case 'c':
+                        System.out.println("Simulating helicopter collision");
+                        break;
+                    case 'E':
+                    case 'e':
+                        System.out.println("Simulating refuel");
+                        break;
+                    case 'G':
+                    case 'g':
+                        System.out.println("Simulating bird collision");
+                        break;
+                    case 'T':
+                    case 't':
+                        System.out.println("Advancing tick");
+                        break;
+                    case 'D':
+                    case 'd':
+                        System.out.println("Generating display");
+                        break;
+                    case 'M':
+                    case 'm':
+                        System.out.println("Showing map");
+                        break;
+                    case 'x':
+                        System.out.println("Exit?");
+                        // ask y/N
+                        gw.exit();
+                        break;
+                    // numbers 1-9
+                }
+            }
+        });
+    }
+}
---
+++ src/org/csc133/a1/GameWorld.java
@@ -0,0 +1,14 @@
+package org.csc133.a1;
+
+public class GameWorld
+{
+    public void init()
+    {
+        // TODO create the initial game objects / setup
+    }
+
+    public void exit()
+    {
+        // TODO exit gracefully
+    }
+}
